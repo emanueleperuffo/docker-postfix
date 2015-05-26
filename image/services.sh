0f97ee21fac74bb1e097031b49f843978c21fb4a
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-26 11:18:12 +0200

    removed syslog remote conf

--- image/services.sh
+++ image/services.sh
@@ -9,6 +9,3 @@ $minimal_apt_get_install postfix postfix-ldap
 ## Setting up posfix service
 mkdir /etc/service/postfix
 cp /build/runit/postfix /etc/service/postfix/run
-
-## Remote syslog
-cp /build/config/syslog-ng/conf.d/* /etc/syslog-ng/conf.d/
