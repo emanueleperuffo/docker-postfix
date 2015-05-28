e12392c00e0116b2d87574f213eec3539e26d8dc
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-28 19:05:41 +0200

    removed syslog file, setting static ssl files

--- image/services.sh
+++ image/services.sh
@@ -4,7 +4,7 @@ source /build/buildconfig
 set -x
 
 ## Installing postfix
-$minimal_apt_get_install postfix postfix-ldap
+$minimal_apt_get_install postfix postfix-ldap spamc
 
 ## Setting up posfix service
 mkdir /etc/service/postfix
